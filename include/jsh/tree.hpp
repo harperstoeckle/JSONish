2b2b703b55c07367b980436362a8338823c22509
Harper <harper.stoeckle@gmail.com>
2020-06-22 15:19:14 -0600

    Improve interface for jsh::Object

--- include/jsh/tree.hpp
+++ include/jsh/tree.hpp
@@ -1,8 +1,11 @@
 #ifndef JSH_TREE_HPP_INCLUDED
 #define JSH_TREE_HPP_INCLUDED
 
+#include <functional>
 #include <map>
+#include <optional>
 #include <string>
+#include <string_view>
 #include <variant>
 #include <vector>
 
@@ -50,9 +53,91 @@ private:
 	std::vector<Value> values_;
 };
 
-/// Contains key-value pairs of strings and jsonish values.
-struct Object
+/** Contains key-value pairs of strings and jsonish values.
+ *
+ * An `Object` does not allow duplicate keys.
+ */
+class Object
 {
+public:
+	Object(void) noexcept = default;
+
+	Object(Object const&) = default;
+	Object(Object&&) noexcept = default;
+
+	Object& operator=(Object const&) = default;
+	Object& operator=(Object&&) noexcept = default;
+
+	[[nodiscard]]
+	auto begin(void) const noexcept { return std::cbegin(values_); }
+	[[nodiscard]]
+	auto end(void) const noexcept { return std::cend(values_); }
+
+	[[nodiscard]]
+	auto begin(void) noexcept { return std::begin(values_); }
+	[[nodiscard]]
+	auto end(void) noexcept { return std::end(values_); }
+
+	/** Attempt to insert a key-value pair into the object.
+	 *
+	 * If a value with the given key already exists in this object,
+	 *
+	 * @return `true` if the key and value were inserted and `false`
+	 * otherwise.
+	 */
+	bool try_insert(std::string key, Value const& value)
+	{
+		return values_.emplace(std::move(key), value).second;
+	}
+	bool try_insert(std::string key, Value&& value)
+	{
+		return values_.emplace(std::move(key), std::move(value)).second;
+	}
+
+	/** Unconditionally set a key and value.
+	 *
+	 * If the key already exists, its value is changed to `value`. If the
+	 * key does not exist, a new key-value pair is created with `key` and
+	 * `value`.
+	 */
+	void set_value(std::string key, Value const& value)
+	{
+		values_.insert_or_assign(std::move(key), value);
+	}
+	void set_value(std::string key, Value&& value)
+	{
+		values_.insert_or_assign(std::move(key), std::move(value));
+	}
+
+	/** Attempt to get the value associated with a key.
+	 *
+	 * If `key` does not exist in this object, produces an empty optional.
+	 * If it does exist, produces an optional containing a
+	 * `std::reference_wrapper` referencing the associated value.
+	 */
+	auto get_value(std::string_view key) const noexcept
+		-> std::optional<std::reference_wrapper<Value const>>
+	{
+		auto const value_pos = values_.find(key);
+		if (value_pos == std::cend(values_))
+		{
+			return std::nullopt;
+		}
+		return value_pos->second;
+	}
+
+private:
+	// Comparator to allow finding elements with `std::string_view`.
+	struct StringViewComparator
+	{
+		using is_transparent = void;
+		bool operator()(
+			std::string_view a, std::string_view b) const noexcept
+		{
+			return std::less<>{}(a, b);
+		}
+	};
+
 	/*
 	 * Every compiler I have tested this with has allowed it. However, I am
 	 * almost completely sure this is undefined behavior, as `Object` (and
@@ -62,7 +147,7 @@ struct Object
 	 * One possible way this could be fixed is to wrap the `Value`s in the
 	 * multimap in `std::unique_ptr`.
 	 */
-	std::multimap<std::string, Value> values_;
+	std::map<std::string, Value, StringViewComparator> values_;
 };
 } // namespace jsh
 
