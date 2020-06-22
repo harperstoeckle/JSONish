170ff78e976446bfe0195cc40fb5f4ffaa834a4d
Harper <harper.stoeckle@gmail.com>
2020-06-22 17:14:40 -0600

    Add some simple tests for the parser

--- tests/parse.test.cpp
+++ tests/parse.test.cpp
@@ -4,3 +4,23 @@
 #include "jsh/tree.hpp"
 
 #include <catch2/catch.hpp>
+
+#include <iostream>
+
+TEST_CASE("Parse jsonish", "[parse]")
+{
+	auto r0 = jsh::parse("");
+	REQUIRE(!r0.is_valid());
+
+	auto r1 = jsh::parse("{}");
+	REQUIRE(r1.is_valid());
+	auto v1 = std::move(r1).value();
+	REQUIRE(std::holds_alternative<jsh::Object>(v1));
+	REQUIRE(std::get<jsh::Object>(v1).is_empty());
+
+	auto r2 = jsh::parse(R"(["one", "two", "three"])");
+	REQUIRE(r2.is_valid());
+	auto v2 = std::move(r2).value();
+	REQUIRE(std::holds_alternative<jsh::List>(v2));
+	REQUIRE(std::get<jsh::List>(v2).size() == 3);
+}
